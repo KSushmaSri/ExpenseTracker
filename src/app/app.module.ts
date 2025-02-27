9faeb80cbc852ab622f16a58fbc2136bae4269da
KSushmaSri <sushmasri1403@gmail.com>
2025-02-27 09:52:04 +0530

    implemented expense service with CRUD operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddExpenseComponent
+    AddExpenseComponent,
+    ExpenseListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -7,5 +8,23 @@ import { HttpClient } from '@angular/common/http'
 })
 export class ExpenseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private baseurl='';
+  getExpenses(): Observable<any>{
+    return this.http.get(`${this.baseurl}`);
+
+  }
+
+  addExpense(expense:any): Observable<any>{
+    return this.http.post(`${this.baseurl}`,expense);
+  }
+
+  updateExpense(id:number,expense:any):Observable<any>{
+    return this.http.put(`${this.baseurl}/${id}`,expense)
+  }
+
+  deleteExpense(id:number):Observable<any>{
+    return this.http.delete(`${this.baseurl}/${id}`);
+  }
 }
